bef1a63415d03bab8e30942e486b88879d4e1a6e
Aadarsha Dhakal <braveboy2058@gmail.com>
2021-06-02 16:57:07 +0545

    Render Overfolow fix

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -21,124 +21,126 @@ class HomePage extends StatelessWidget {
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            FutureBuilder(
-                future: archiveService.getCurrentCode(),
-                builder: (context, snapshot) {
-                  return CodeEditor(
-                    disableNavigationbar: true,
-                    edit: false,
-                    model: EditorModel(
-                      styleOptions: EditorModelStyleOptions(
-                        heightOfContainer:
-                            MediaQuery.of(context).size.height - 180,
-                      ),
-                      files: [
-                        FileEditor(
-                          name: 'main.dart',
-                          language: 'dart',
-                          code: snapshot.data != null
-                              ? snapshot.data.toString()
-                              : '',
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              FutureBuilder(
+                  future: archiveService.getCurrentCode(),
+                  builder: (context, snapshot) {
+                    return CodeEditor(
+                      disableNavigationbar: true,
+                      edit: true,
+                      model: EditorModel(
+                        styleOptions: EditorModelStyleOptions(
+                          heightOfContainer:
+                              MediaQuery.of(context).size.height - 180,
                         ),
-                      ],
-                    ),
-                    onSubmit: (String? language, String? value) async {
-                      await ArchiveService.getInstance.writeCounter(value!);
-                      try {
-                        var file =
-                            await ArchiveService.getInstance.getTarFile();
-                        BlocProvider.of<FunctionBloc>(context).add(
-                          FunctionSaveButtonPressedEvent(
-                            file: file!,
+                        files: [
+                          FileEditor(
+                            name: 'main.dart',
+                            language: 'dart',
+                            code: snapshot.data != null
+                                ? snapshot.data.toString()
+                                : '',
                           ),
-                        );
-                      } catch (e) {
-                        print(e);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Error Occurred'),
-                          ),
-                        );
-                      }
-                    },
-                  );
-                }),
-            const SizedBox(
-              height: 20.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  BlocProvider.of<FunctionBloc>(context).add(
-                    FunctionRunButtonPressedEvent(),
-                  );
-                  showModalBottomSheet(
-                      context: context,
-                      builder: (_) {
-                        return BlocProvider.value(
-                          value: BlocProvider.of<FunctionBloc>(context),
-                          child: Container(
-                            color: Colors.black87,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                const Padding(
-                                  padding: EdgeInsets.all(3.0),
-                                  child: Text(
-                                    'Output',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18.0,
+                        ],
+                      ),
+                      onSubmit: (String? language, String? value) async {
+                        await ArchiveService.getInstance.writeCounter(value!);
+                        try {
+                          var file =
+                              await ArchiveService.getInstance.getTarFile();
+                          BlocProvider.of<FunctionBloc>(context).add(
+                            FunctionSaveButtonPressedEvent(
+                              file: file!,
+                            ),
+                          );
+                        } catch (e) {
+                          print(e);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Error Occurred'),
+                            ),
+                          );
+                        }
+                      },
+                    );
+                  }),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    BlocProvider.of<FunctionBloc>(context).add(
+                      FunctionRunButtonPressedEvent(),
+                    );
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (_) {
+                          return BlocProvider.value(
+                            value: BlocProvider.of<FunctionBloc>(context),
+                            child: Container(
+                              color: Colors.black87,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.all(3.0),
+                                    child: Text(
+                                      'Output',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18.0,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                const Divider(color: Colors.white),
-                                Container(
-                                  height: 200,
-                                  child:
-                                      BlocBuilder<FunctionBloc, FunctionState>(
-                                    builder: (context, state) {
-                                      if (state
-                                          is FunctionExecutionRunningState) {
-                                        return const Center(
-                                          child: CircularProgressIndicator(),
-                                        );
-                                      } else if (state
-                                          is FunctionExecutionErrorState) {
-                                        return Text(
-                                          state.message,
-                                          style: const TextStyle(
-                                            color: Colors.red,
-                                          ),
-                                        );
-                                      } else if (state
-                                          is FunctionExecutionSuccessState) {
-                                        return Text(
-                                          state.message ?? 'Unknown Error',
-                                          style: const TextStyle(
-                                            color: Colors.white,
-                                          ),
-                                        );
-                                      } else {
-                                        return Container();
-                                      }
-                                    },
-                                  ),
-                                )
-                              ],
+                                  const Divider(color: Colors.white),
+                                  Container(
+                                    height: 200,
+                                    child: BlocBuilder<FunctionBloc,
+                                        FunctionState>(
+                                      builder: (context, state) {
+                                        if (state
+                                            is FunctionExecutionRunningState) {
+                                          return const Center(
+                                            child: CircularProgressIndicator(),
+                                          );
+                                        } else if (state
+                                            is FunctionExecutionErrorState) {
+                                          return Text(
+                                            state.message,
+                                            style: const TextStyle(
+                                              color: Colors.red,
+                                            ),
+                                          );
+                                        } else if (state
+                                            is FunctionExecutionSuccessState) {
+                                          return Text(
+                                            state.message ?? 'Unknown Error',
+                                            style: const TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          );
+                                        } else {
+                                          return Container();
+                                        }
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      });
-                },
-                child: const Text('Run'),
+                          );
+                        });
+                  },
+                  child: const Text('Run'),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
